8052553359fa8fc049bab3eeb5614f6a48cca59d
trevorfaivre <trevorfaivre@hotmail.com>
2022-05-21 16:47:17 -0500

    FIN

--- server.js
+++ server.js
@@ -1,27 +1,40 @@
 
 const express = require('express');
+const { use } = require('express/lib/application');
+const bodyParser = require('body-parser')
 const app = express();
 const budget = require('./models/budgets');
 //middleware
-app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('./public'));
 
-app.get('/', (request, response) => {
-    response.send('Welcome to Budgetr')
-});
+// app.use((request, response) => {
+//     console.log('I run for all routes')
+//     next()
+// });
 
 //Index
 app.get('/budgets', (request, response) => {
     response.render('index.ejs', { allBudget: budget })
 });
 
-// New
-// app.get('/budgets/:index', (request, response) => {
-//     response.send(budget[request.params.index])
+// app.get('/budgets', (req, res) => {
+//     res.render('/budgets', {
+//         user : budgets.CreateExpense
 //     });
+// });
+
+// New
+app.get('/budgets/new', (request, response) => {
+    response.render('new.ejs');
+});
+
 
 //Post
 app.post('/budgets', (request, response) => {
-    response.send('sup')
+    console.log(request.body)
+    budget.push(request.body)
+    response.redirect('/budgets')
 });
 
 //Show
